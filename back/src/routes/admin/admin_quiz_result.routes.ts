5540a9f0ebb97eab47e4940594c87ef60ce5d744
ioamra <iom.monchau@gmail.com>
2024-11-06 03:26:33 +0100

    add haveAdminToken middleware

--- back/src/routes/admin/admin_quiz_result.routes.ts
+++ back/src/routes/admin/admin_quiz_result.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { create, remove, update } from '../../controllers/admin/admin_quiz_result.controller';
+import haveAdminToken from '../../middleware/haveAdminToken.middleware';
 
 const router = Router();
 
-router.post('/create', create);
-router.put('/update', update);
-router.delete('/remove', remove);
+router.post('/create', haveAdminToken, create);
+router.put('/update', haveAdminToken, update);
+router.delete('/remove', haveAdminToken, remove);
 
 export default router;
