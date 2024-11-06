ec57bf835175336e31efe15d07deece3a881661f
koodooo <nayadsaidali@gmail.com>
2024-11-06 16:00:10 +0100

    modif admin avatar controller

--- back/src/controllers/admin/admin_avatar.controller.ts
+++ back/src/controllers/admin/admin_avatar.controller.ts
@@ -34,22 +34,56 @@ export const create = [
   },
 ];
 
-export const update = async (req: Request, res: Response) => {
+export const update = [
+  upload.single('file'),
+  async (req: Request, res: Response) => {
   try {
-    const { rows } = await pool.query("SELECT 'AAAAAAAAAAAAAAAAAAAA' AS prout");
+    const id = parseInt(req.params.id);
+    if (!req.file) {
+      res.status(400).json({ message: 'No file uploaded' });
+      return;
+    }
+    const { originalname, buffer, mimetype } = req.file;
+    const allowedMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+    const uploadPath = path.join(__dirname, '../../upload/avatar', originalname);
 
-    res.status(201).json(rows);
+    if (!allowedMimeTypes.includes(mimetype)) {
+      res.status(400).json({ message: 'Invalid file type. Only PNG, JPG, and JPEG are allowed.' });
+      return;
+    }
+    const { rows } = await pool.query("SELECT img FROM avatar WHERE id = $1", [id]);
+    const oldPath = path.join(__dirname, '../../upload/avatar', rows[0].img);
+    // Delete the old file
+    fs.unlinkSync(oldPath);
+    // Save the file to the upload directory
+    fs.writeFileSync(uploadPath, buffer);
+
+
+    await pool.query("UPDATE avatar SET img = $1 WHERE id = $2", [originalname, id]);
+    res.status(201).json({ message: 'Avatar has been update' });
   } catch (error) {
     console.error('Error :' + error);
     res.status(500).json({ message: 'Internal server error' });
   }
-};
+}];
 
 export const remove = async (req: Request, res: Response) => {
   try {
-    const { rows } = await pool.query("SELECT 'AAAAAAAAAAAAAAAAAAAA' AS prout");
 
-    res.status(201).json(rows);
+    if (!req.file) {
+      res.status(400).json({ message: 'No file uploaded' });
+      return;
+    }
+
+    const id = parseInt(req.params.id);
+    const { originalname, buffer, mimetype } = req.file;
+    const uploadPath = path.join(__dirname, '../../upload/avatar', originalname);
+    const { rows } = await pool.query("DELETE from avatar WHERE id = $1", [id]);
+
+    fs.unlinkSync(uploadPath);
+    
+
+    res.status(201).json({ message: 'Avatar has been delete' });
   } catch (error) {
     console.error('Error :' + error);
     res.status(500).json({ message: 'Internal server error' });
