a32cb7b6dc77725f6e1404cdaefbe6728be92495
Alexis <garibaldialexis@gmail.com>
2024-11-07 19:24:10 +0100

    quiz and logo

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -11,8 +11,8 @@ import Register from './pages/auth/Register';
 import Lesson from './pages/lessons/Lesson';
 import Lessons from './pages/lessons/Lessons';
 import Profile from './pages/profile/Profile';
-import Quiz from './pages/quizzes/Quiz';
-import QuizPage from './pages/quizzes/QuizPage'; // Assurez-vous que le chemin d'import est correct
+// import Quiz from './pages/quizzes/Quiz';
+import QuizPage from './pages/quizzes/QuizPage';
 import Quizzes from './pages/quizzes/Quizzes';
 import Friends from './pages/social/Friends';
 import Leaderboard from './pages/social/Leaderboard';
@@ -28,8 +28,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/quizzes" element={<Quizzes />} />
-            <Route path="/quiz/:id" element={<Quiz />} />
-            <Route path="/quiz/:id" element={<QuizPage />} /> {/* Route pour le quiz */}
+            {/* <Route path="/quiz/:id" element={<Quiz />} /> */}
+            <Route path="/quiz/:id" element={<QuizPage />} />
             <Route path="/lessons" element={<Lessons />} />
             <Route path="/lesson/:id" element={<Lesson />} />
             <Route path="/profile" element={<Profile />} />
