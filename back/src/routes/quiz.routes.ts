458bee1f4cc1150e2fb01f5ab2f94a9ef443918f
ioamra <iom.monchau@gmail.com>
2024-11-07 16:54:21 +0100

    quiz add

--- back/src/routes/quiz.routes.ts
+++ back/src/routes/quiz.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { addQuizResult, findAll, findOne } from '../controllers/quiz.controller';
+import haveToken from '../middleware/haveToken.middleware';
 
 const router = Router();
 
 router.get('/', findAll);
 router.get('/:id', findOne);
-router.post('/addQuizResult/:id', addQuizResult);
+router.post('/add-result/:id', haveToken, addQuizResult);
 
 export default router;
